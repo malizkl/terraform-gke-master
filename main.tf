ddc8f4906e536c1ad99fd86377e3db4be6b8085d
malizkl <63977569+malizkl@users.noreply.github.com>
2021-11-29 11:31:21 +0300

    first commit

---
+++ main.tf
@@ -0,0 +1,50 @@
+resource "google_container_cluster" "default" {
+  name        = var.name
+  project     = var.project
+  description = "Demo GKE Cluster"
+  location    = var.location
+
+  remove_default_node_pool = true
+  initial_node_count       = var.initial_node_count
+
+  master_auth {
+    username = ""
+    password = ""
+
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+}
+
+resource "google_container_node_pool" "default" {
+  name       = "${var.name}-node-pool"
+  project    = var.project
+  location   = var.location
+  cluster    = google_container_cluster.default.name
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = var.machine_type
+
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
+
+resource "google_artifact_registry_repository" "my-repo" {
+  provider = google-beta
+
+  location = "us-central1"
+  repository_id = "my-repository"
+  description = "example docker repository"
+  format = "DOCKER"
+}
+
